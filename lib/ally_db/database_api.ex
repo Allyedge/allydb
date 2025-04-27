081c1b56e41a7c60ad4f07ae0e37979c6c30d7c1
Allyedge <alimarslank@gmail.com>
2025-04-27 14:32:49 +0200

    Implement commands for database api

--- lib/ally_db/database_api.ex
+++ lib/ally_db/database_api.ex
@@ -8,8 +8,18 @@ defmodule AllyDB.DatabaseAPI do
 
   alias AllyDB.Core.ProcessManager
   alias AllyDB.ShardActor
+  alias AllyDB.Sharding
 
   require Logger
+
+  @typedoc "The key used in the database."
+  @type key :: any()
+
+  @typedoc "The value stored in the database."
+  @type value :: any()
+
+  @typedoc "Possible error reasons returned by API functions."
+  @type error_reason :: :not_found | :shard_unavailable | {:shard_crash, any()}
 
   @num_shards Application.compile_env(:allydb, :num_shards, 16)
   if !is_integer(@num_shards) or @num_shards < 1 do
@@ -39,5 +49,85 @@ defmodule AllyDB.DatabaseAPI do
 
     Logger.info("Shard actors startup sequence complete.")
     :ok
+  end
+
+  @doc """
+  Retrieves the value associated with the given `key`.
+
+  Routes the request to the appropriate shard actor.
+  Returns `{:ok, value}` or `{:error, reason}` where reason includes
+  `:not_found`, `:shard_unavailable`, or `{:shard_crash, exit_reason}`.
+  """
+  @spec get(key()) :: {:ok, value()} | {:error, error_reason()}
+  def get(key) do
+    case find_shard_pid(key) do
+      {:ok, pid} ->
+        try do
+          GenServer.call(pid, {:get, key})
+        rescue
+          reason ->
+            Logger.error("Shard actor call exited for key '#{inspect key}'. PID: #{inspect pid}, Reason: #{inspect reason}")
+            {:error, {:shard_crash, reason}}
+        end
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  @doc """
+  Sets the `value` for the given `key`.
+
+  Routes the request to the appropriate shard actor. This operation is
+  asynchronous from the caller's perspective. Returns `:ok` if the
+  responsible shard is found (message sent), `{:error, :shard_unavailable}` otherwise.
+  """
+  @spec set(key(), value()) :: :ok | {:error, :shard_unavailable}
+  def set(key, value) do
+    case find_shard_pid(key) do
+      {:ok, pid} ->
+        GenServer.cast(pid, {:set, key, value})
+        :ok
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  @doc """
+  Deletes the given `key` and its associated value.
+
+  Routes the request to the appropriate shard actor. This operation is
+  asynchronous from the caller's perspective. Returns `:ok` if the
+  responsible shard is found (message sent), `{:error, :shard_unavailable}` otherwise.
+  """
+  @spec delete(key()) :: :ok | {:error, :shard_unavailable}
+  def delete(key) do
+    case find_shard_pid(key) do
+      {:ok, pid} ->
+        GenServer.cast(pid, {:delete, key})
+        :ok
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  @spec find_shard_pid(key()) :: {:ok, pid()} | {:error, :shard_unavailable}
+  defp find_shard_pid(key) do
+    shard_id = Sharding.hash_key_to_shard(key, @num_shards)
+    shard_process_id = "shard_#{shard_id}"
+
+    case ProcessManager.lookup_process(shard_process_id) do
+      [{pid, _value}] ->
+        {:ok, pid}
+
+      [] ->
+        Logger.error(
+          "Cannot find shard actor PID for key '#{inspect(key)}' (shard_id: #{shard_id}, registry_id: '#{shard_process_id}')"
+        )
+
+        {:error, :shard_unavailable}
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule AllyDB.MixProject do
 
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :observer, :wx],
       mod: {AllyDB.Application, []}
     ]
   end
